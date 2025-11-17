dc3fe373ca11d4eb60fc7fd717980a72707d7cb8
Ric <rachim27reuel@gmail.com>
2025-11-17 09:47:01 +0530

    Update for linking files

--- frontend/src/admin-nav.jsx
+++ frontend/src/admin-nav.jsx
@@ -19,7 +19,7 @@ return (
 
 export const Browser = () => {
 return (
-<BrowserRouter>
+<BrowserRouter basename = "/admin">
     <NavBar /> <br />
     <Routes>
     <Route path = "/reportcards" element = {<ReportCards />} />

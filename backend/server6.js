bb15b53d8ec1a63cc76daa541b8bfa8877636702
Ric <rachim27reuel@gmail.com>
2026-03-25 09:20:38 +0530

    Fixing SSL issue

--- backend/server6.js
+++ backend/server6.js
@@ -28,7 +28,7 @@ const users = JSON.parse(fs.readFileSync(usersPath, "utf8"));
 const { Pool } = pg;
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  //ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false }
 });
 
 /* ---------------------------------------------------

40a08fdd12aca30474ea48c7a1be51386c805dd5
Ric <rachim27reuel@gmail.com>
2026-03-26 00:46:11 +0530

    Hope it's final.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [react()],
-  root: "./",  // <-- points to your index.html
+  plugins: [react(), viteSingleFile()],
+  root: "./",
   base: "",
   build: {
-    outDir: path.resolve(__dirname, "dis"),  // <-- build goes here
-    emptyOutDir: true
-  }
+    outDir: path.resolve(__dirname, "dis"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
 });
-

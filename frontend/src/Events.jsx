656f13e1be0eeb59dfa7145b49608843f485967b
Ric <rachim27reuel@gmail.com>
2026-03-30 23:35:56 +0530

    Updating events page

--- frontend/src/Events.jsx
+++ frontend/src/Events.jsx
@@ -158,7 +158,7 @@ export function EventSubmission() {
         {ev.images.map((url, i) => (
           <img
             key={i}
-            src={img}
+            src={url}
             alt={ev.title}
             width="150"
           />

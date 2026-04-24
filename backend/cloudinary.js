6d7c2bf9f3358adfda8821d73f458683cb4d68fe
Ric <rachim27reuel@gmail.com>
2026-04-25 01:16:10 +0530

    Updating Cloudinary details

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -1,10 +1,9 @@
-// cloudinary.js
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
